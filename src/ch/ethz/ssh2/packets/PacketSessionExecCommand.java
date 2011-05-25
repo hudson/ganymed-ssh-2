05ea29edc8938d4d3a44a7116c6f1d8317e260ae
cleondris@gmail.com <cleondris@gmail.com@1e9665cc-1c9b-f8ef-25e3-9e2d187d030b>
2011-05-25 14:40:45 +0000

    Initial checkin.
    git-svn-id: http://ganymed-ssh-2.googlecode.com/svn/trunk@2 1e9665cc-1c9b-f8ef-25e3-9e2d187d030b

---
+++ src/ch/ethz/ssh2/packets/PacketSessionExecCommand.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2006-2011 Christian Plattner. All rights reserved.
+ * Please refer to the LICENSE.txt for licensing details.
+ */
+package ch.ethz.ssh2.packets;
+
+/**
+ * PacketSessionExecCommand.
+ * 
+ * @author Christian Plattner
+ * @version 2.50, 03/15/10
+ */
+public class PacketSessionExecCommand
+{
+	byte[] payload;
+
+	public int recipientChannelID;
+	public boolean wantReply;
+	public String command;
+
+	public PacketSessionExecCommand(int recipientChannelID, boolean wantReply, String command)
+	{
+		this.recipientChannelID = recipientChannelID;
+		this.wantReply = wantReply;
+		this.command = command;
+	}
+	
+	public byte[] getPayload()
+	{
+		if (payload == null)
+		{
+			TypesWriter tw = new TypesWriter();
+			tw.writeByte(Packets.SSH_MSG_CHANNEL_REQUEST);
+			tw.writeUINT32(recipientChannelID);
+			tw.writeString("exec");
+			tw.writeBoolean(wantReply);
+			tw.writeString(command);
+			payload = tw.getBytes();
+		}
+		return payload;
+	}
+}
