2779d9ec4367977106773275f88531e542f5b8d3
dkocher@sudo.ch <dkocher@sudo.ch@1e9665cc-1c9b-f8ef-25e3-9e2d187d030b>
2011-06-02 10:19:02 +0000

    Fix issue 7.
    git-svn-id: http://ganymed-ssh-2.googlecode.com/svn/trunk@40 1e9665cc-1c9b-f8ef-25e3-9e2d187d030b

--- src/main/java/ch/ethz/ssh2/util/StringEncoder.java
+++ src/main/java/ch/ethz/ssh2/util/StringEncoder.java
@@ -5,62 +5,26 @@
  */
 package ch.ethz.ssh2.util;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 /**
- * 
  * @author Christian Plattner
- * @version 2.50, 03/15/10
+ * @version $Id$
  */
 public class StringEncoder
 {
 	public static byte[] GetBytes(String data)
 	{
-		try
-		{
-			return data.getBytes("ISO8859_1");
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			byte[] bytes = new byte[data.length()];
-
-			for (int i = 0; i < data.length(); i++)
-			{
-				char c = data.charAt(i);
-				if (c > 127)
-					bytes[i] = 0x3F; // ?
-				else
-					bytes[i] = (byte) c;
-			}
-
-			return bytes;
-		}
+		return data.getBytes(Charset.forName("UTF-8"));
 	}
 
 	public static String GetString(byte[] data)
 	{
 		return GetString(data, 0, data.length);
 	}
-	
+
 	public static String GetString(byte[] data, int off, int len)
 	{
-		try
-		{
-			return new String(data, off, len, "ISO8859_1");
-		}
-		catch (UnsupportedEncodingException e)
-		{
-			char[] chars = new char[len];
-
-			for (int i = 0; i < len; i++)
-			{
-				char c = (char) data[off + i];
-				if (c > 127)
-					c = '?';
-				chars[i] = c;
-			}
-
-			return new String(chars);
-		}
+		return new String(data, off, len, Charset.forName("UTF-8"));
 	}
 }
